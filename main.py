8d5c18721d7b97410ba27a9f98481fc58a740c55
AbhyudaySrivatsa <abhyuday.srivatsa@gmail.com>
2025-04-12 12:36:06 -0400

    small updates

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import requests
 import uuid
 import os
 
-UPLOAD_FOLDER = '/uploads/'
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
 ALLOWED_EXTENSIONS = {'txt', 'pdf'}
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "goon"
@@ -19,10 +19,9 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 def too_large(e):
     return make_response(jsonify(message="File is too large"), 413)
 
-@app.route('/templates/home.html', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == "POST":
-        print(request.files)
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -33,26 +32,27 @@ def home():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(str(uuid.uuid4()))
-            file.save(os.path.join("." + app.config['UPLOAD_FOLDER'], filename + '.pdf'))
-            return redirect('/')
+            os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename + '.pdf'))
+            return redirect('/conversation')
     else:
         return render_template("home.html")
 
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/templates/conversation.html')
+@app.route('/conversation')
 def upload():
     return render_template("conversation.html")
 
-@app.route('/templates/tinder.html')
+@app.route('/tinder')
 def tinder():
     return render_template("tinder.html")
 
-@app.route('/templates/schedule.html')
+@app.route('/schedule')
 def schedule():
     return render_template("schedule.html")
 
